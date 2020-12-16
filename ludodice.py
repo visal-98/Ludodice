7ae65143c77f6ce286b423b1585ec0968776d552
Visal Hazra <62242429+visal-98@users.noreply.github.com>
2020-12-16 23:10:09 +0530

    Create ludodice.py
    Dice For Ludo and Snake Game

---
+++ ludodice.py
@@ -0,0 +1,101 @@
+import random 
+
+  
+
+  
+
+x = "r"
+
+   
+
+while x == "r": 
+
+      
+
+    # Gnenerates a random number 
+
+    # between 1 and 6 (including 
+
+    # both 1 and 6) 
+
+    no = random.randint(1,6) 
+
+      
+
+    if no == 1: 
+
+        print(" ----- ") 
+
+        print("[     ]") 
+
+        print("[  0  ]") 
+
+        print("[     ]") 
+
+        print(" ----- ") 
+
+    if no == 2: 
+
+        print(" ----- ") 
+
+        print("[ 0   ]") 
+
+        print("[     ]") 
+
+        print("[   0 ]") 
+
+        print(" ----- ") 
+
+    if no == 3: 
+
+        print(" ----- ") 
+
+        print("[     ]") 
+
+        print("[0 0 0]") 
+
+        print("[     ]") 
+
+        print(" ----- ") 
+
+    if no == 4: 
+
+        print(" ----- ") 
+
+        print("[0   0]") 
+
+        print("[     ]") 
+
+        print("[0   0]") 
+
+        print(" ----- ") 
+
+    if no == 5: 
+
+        print(" ----- ") 
+
+        print("[0   0]") 
+
+        print("[  0  ]") 
+
+        print("[0   0]") 
+
+        print(" ----- ") 
+
+    if no == 6: 
+
+        print(" ----- ") 
+
+        print("[0 0 0]") 
+
+        print("[     ]") 
+
+        print("[0 0 0]") 
+
+        print(" ----- ") 
+
+          
+
+    x=input("press r to roll again and X to exit:") 
+
+    print("\n")
